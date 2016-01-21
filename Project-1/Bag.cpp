e42c40789725b5ed610afb28d8e618b1c44551e8
Jimmy Liu <sicong.liu98@gmail.com>
2016-01-20 16:26:43 -0800

    working

--- Project-1/Bag.cpp
+++ Project-1/Bag.cpp
@@ -31,7 +31,7 @@ public:
 	and the count of items in the bag has decreased by 1.
 	@param anEntry  The entry to be removed.
 	@return  True if removal was successful, or false if not. */
-	virtual bool remove(const T& anEntry) = 0;
+	//virtual bool remove(const T& anEntry) = 0;
 
 	/** Removes all entries from this bag.
 	@post  Bag contains no items, and the count of items is 0. */
@@ -54,6 +54,7 @@ class Node
 private:
 	T item; // A data item
 	shared_ptr<Node<T> > next; // Pointer to next node
+	shared_ptr<Node<T> > prev; // Pointer to the previous node
 public:
 	Node() : next(nullptr)
 	{
@@ -72,6 +73,9 @@ public:
 	{
 		next = nextNodePtr;
 	} // end setNext
+	void setPrev(shared_ptr<Node<T> > prevNodePtr) {
+		prev = prevNodePtr;
+	}
 	T getItem() const
 	{
 		return item;
@@ -80,6 +84,9 @@ public:
 	{
 		return next;
 	} // end getNext
+	shared_ptr<Node<T> > getPrev() const {
+		return prev;
+	}
 	T operator *()
 	{
 		return getItem();
@@ -92,51 +99,18 @@ public:
 }; // end Node
 
 template<typename T>
-class LinkedBag : public BagInterface<T>
+class DoublyLinkedBag : public BagInterface<T>
 {
 private:
 	shared_ptr<Node<T> > headPtr; // Pointer to first node
+	shared_ptr<Node<T> > tailPtr; // Pointer to the last node
 	int itemCount;           // Current count of bag items
 							 // Returns either a pointer to the node containing a given entry
 							 // or the null pointer if the entry is not in the bag.
 public:
-	LinkedBag() : headPtr(nullptr), itemCount(0)
+	DoublyLinkedBag() : headPtr(nullptr), itemCount(0)
 	{
 	}  // end default constructor
-	LinkedBag(const LinkedBag<T>& aBag)
-	{
-		itemCount = aBag.itemCount;
-		shared_ptr<Node<T> > origChainPtr = aBag.headPtr;  // Points to nodes in original chain
-
-		if (origChainPtr == nullptr)
-			headPtr = nullptr;  // Original bag is empty
-		else
-		{
-			// Copy first node
-			headPtr = shared_ptr<Node<T> >(new Node<T>());
-			headPtr->setItem(origChainPtr->getItem());
-
-			// Copy remaining nodes
-			shared_ptr<Node<T> > newChainPtr = headPtr;      // Points to last node in new chain
-			origChainPtr = origChainPtr->getNext();     // Advance original-chain pointer
-
-			while (origChainPtr != nullptr)
-			{
-				// Get next item from original chain
-				T nextItem = origChainPtr->getItem();
-				// Create a new node containing the next item
-				shared_ptr<Node<T> > newNodePtr(new Node<T>(nextItem));
-				// Link new node to end of new chain
-				newChainPtr->setNext(newNodePtr);
-				// Advance pointer to new last node
-				newChainPtr = newChainPtr->getNext();
-				// Advance original-chain pointer
-				origChainPtr = origChainPtr->getNext();
-			}  // end while
-
-			newChainPtr->setNext(nullptr);              // Flag end of chain
-		}  // end if
-	}  // end copy constructor
 	bool isEmpty() const
 	{
 		return itemCount == 0;
@@ -148,31 +122,23 @@ public:
 	bool add(const T& newEntry)
 	{
 		// Add to beginning of chain: new node references rest of chain;
-		// (headPtr is null if chain is empty)        
+		// (headPtr is null if chain is empty)    
 		shared_ptr<Node<T> > nextNodePtr(new Node<T>(newEntry));
+		if (headPtr != nullptr) {
+			headPtr->setPrev(nextNodePtr);
+		}
 		nextNodePtr->setNext(headPtr);  // New node points to chain
 										//   Node<ItemType>* nextNodePtr = new Node<ItemType>(newEntry, headPtr); // alternate code
-
+		if (itemCount == 0) {
+			tailPtr = nextNodePtr;
+		} else if(itemCount == 1) {
+			tailPtr = headPtr;
+		}
 		headPtr = nextNodePtr;          // New node is now first node
 		itemCount++;
 
 		return true;
 	}  // end add
-	bool remove(const T& anEntry)
-	{
-		shared_ptr<Node<T> > entryNodePtr = shared_ptr<Node<T> >(getPointerTo(anEntry));
-		bool canRemoveItem = !isEmpty() && (entryNodePtr != nullptr);
-		if (canRemoveItem)
-		{
-			// Copy data from first node to located node
-			entryNodePtr->setItem(headPtr->getItem());
-			// Delete first node
-			shared_ptr<Node<T> > nodeToDeletePtr = headPtr;
-			headPtr = headPtr->getNext();
-			itemCount--;
-		} // end if
-		return canRemoveItem;
-	}  // end remove
 	void clear()
 	{
 		itemCount = 0;
@@ -213,7 +179,7 @@ public:
 
 		return curPtr;
 	} // end getPointerTo
-	friend ostream& operator << (ostream& stream, LinkedBag<string>& bag)
+	/*friend ostream& operator << (ostream& stream, DoublyLinkedBag<string>& bag)
 	{
 		stream << "The bag contains " << bag.getCurrentSize() << " items:" << endl;
 		shared_ptr<Node<T> > rover = bag.headPtr;
@@ -224,38 +190,29 @@ public:
 		}  // end for
 		cout << endl << endl;
 		return stream;
-	}  // end displayBag
-}; // end LinkedBag
-
-void copyConstructorTester()
-{
-	LinkedBag<string> bag;
-	string numbers[] = { "zero", "one", "two", "three", "four", "five" };
-	for (int i = 0; i < bag.getCurrentSize(); i++)
+	}  // end displayBag*/
+	friend ostream& operator << (ostream& stream, DoublyLinkedBag<string>& bag)
 	{
-		cout << "Adding " << numbers[i] << endl;
-		bool success = bag.add(numbers[i]);
-		if (!success)
-			cout << "Failed to add " << numbers[i] << " to the bag." << endl;
-	}
-	cout << bag;
-
-	LinkedBag<string> copyOfBag = bag;
-	cout << "Copy of bag: ";
-	cout << copyOfBag;
-
-	cout << "The copied bag: ";
-	cout << bag;
-}  // end copyConstructorTester
+		stream << "The bag contains " << bag.getCurrentSize() << " items:" << endl;
+		shared_ptr<Node<T> > rover = bag.tailPtr;
+		while (rover != nullptr)
+		{
+			cout << *rover << BLANK;
+			rover = rover->getPrev();
+		}  // end for
+		cout << endl << endl;
+		return stream;
+	}  // end displayBag
+}; // end DoublyLinkedBag
 
 void bagTester() 
 {
-	LinkedBag<string> bag;
+	DoublyLinkedBag<string> bag;
 	cout << "Testing the Link-Based Bag:" << endl;
 	cout << "isEmpty: returns " << bag.isEmpty() << "; should be 1 (true)" << endl;
 	cout << bag;
 
-	string numbers[] = { "one", "two", "three", "four", "five", "one" };
+	string numbers[] = { "one", "two", "three", "four", "five"};
 	cout << "Add 6 items to the bag: " << endl;
 	for (int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++)
 	{
@@ -268,7 +225,6 @@ void bagTester()
 
 int main()
 {
-	copyConstructorTester();
 	bagTester();
 	return 0;
 }  // end main
